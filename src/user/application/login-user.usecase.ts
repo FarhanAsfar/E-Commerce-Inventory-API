419b5e692ca205789d6a89d1f14d12f898a32d9a
arnob <farhanasfar21@gmail.com>
2025-09-23 22:18:30 +0600

    Add login user-case

--- src/user/application/login-user.usecase.ts
+++ src/user/application/login-user.usecase.ts
@@ -5,26 +5,27 @@ import { UserRepository } from "../domain/user.repository";
 
 @Injectable()
 export class LoginUserUseCase {
-  constructor(
-    private readonly userRepository: UserRepository,
-    private readonly jwtService: JwtService,
-  ) {}
+    constructor(
+        private readonly userRepository: UserRepository,
+        private readonly jwtService: JwtService,
+    ) {}
 
-  async execute(email: string, password: string): Promise<{ access_token: string }> {
-    const user = await this.userRepository.findByEmail(email);
-    if (!user) {
-      throw new UnauthorizedException('User not found');
+    async execute(email: string, password: string): Promise<{ access_token: string }> {
+        const user = await this.userRepository.login(email);
+        if (!user) {
+        throw new UnauthorizedException('User not found');
+        }
+
+        // comparing hashed password
+        const isValid = await bcrypt.compare(password, user.password);
+        
+        if (!isValid) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        const payload = { id: user.id, email: user.email };
+        const token = await this.jwtService.signAsync(payload);
+
+        return { access_token: token };
     }
-
-    // comparing hashed password
-    const isValid = await bcrypt.compare(password, user.password);
-    if (!isValid) {
-      throw new UnauthorizedException('Invalid credentials');
-    }
-
-    const payload = { id: user.id, email: user.email };
-    const token = await this.jwtService.signAsync(payload);
-
-    return { access_token: token };
-  }
 }
