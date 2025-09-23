9aeb7e7937e489cfbd1a35d232db863c6e071240
arnob <farhanasfar21@gmail.com>
2025-09-23 22:19:24 +0600

    Add register user-case

--- src/user/application/register-user.usecase.ts
+++ src/user/application/register-user.usecase.ts
@@ -1,17 +1,19 @@
-import { ConflictException, Injectable } from "@nestjs/common";
+import { ConflictException, Inject, Injectable } from "@nestjs/common";
 import { UserRepository } from "../domain/user.repository";
-import { CreateUserDto } from "./dto/register.dto";
+import type { CreateUserDto } from "./dto/register.dto";
 import { User } from "../domain/user.entity";
 import * as bcrypt from "bcrypt"
 
 
 @Injectable()
 export class RegisterUserUseCase {
-    constructor(private readonly userRepository: UserRepository) {}
+    constructor(
+        @Inject(UserRepository)
+        private readonly userRepository: UserRepository) {}
 
     async execute(createUserDto: CreateUserDto): Promise<User> {
         //checking for existing email
-        const exists = await this.userRepository.findByEmail(createUserDto.email);
+        const exists = await this.userRepository.login(createUserDto.email);
 
         if(exists){
             throw new ConflictException("Email already exists");
